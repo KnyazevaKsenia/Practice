8c75a51f0078a6bee98f8a2bb55567e38588f54e
ksenia <kn_ksusha@mail.ru>
2024-06-30 23:21:24 +0300

    Class Car created

--- app/src/main/java/com/example/raceshw/MainActivity.kt
+++ app/src/main/java/com/example/raceshw/MainActivity.kt
@@ -2,47 +2,91 @@ package com.example.raceshw
 
 import android.os.Bundle
 import android.webkit.ConsoleMessage
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.raceshw.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
+    private var button:Button?= null
+    private var text: TextView?=null
+    private var viewBinding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding?.root)
+        viewBinding=ActivityMainBinding.inflate(layoutInflater)
+        initViews()
     }
-
     override fun onDestroy() {
         super.onDestroy()
     }
+    private fun initViews()
+    {
+        viewBinding?.let{ binding->
+            binding.startButton.setOnClickListener(){
+                startRace()
+            }
+        }
+    }
 }
+
+fun startRace(){}
+
 
 open class Car(
     val mark: String,
     val model:Int,
     val year: Int,
-    val colour:String)
+    val color:String)
 {
     fun printInfo()
     {
-
+        println("Model: $model, Mark: $mark, Year: $year, Color: $color")
     }
-
 }
 
-class Jeep(mark: String,
-           model: Int,
-           year: Int,
-           colour: String
-            ): Car(
+class Jeep(mark: String, model: Int, year: Int, color: String, val wheelWidth:Int): Car(
     mark=mark,
     model=model,
     year=year,
-    colour=colour
+    color=color
+)
+{
+
+}
+
+class Crossover(mark: String, model: Int, year: Int, color: String, val driveUnit:String): Car(
+    mark=mark,
+    model=model,
+    year=year,
+    color=color
+)
+{
+
+}
+
+class Pickup(mark: String, model: Int, year: Int, color: String, val liflingCapacity:Int ): Car(
+    mark=mark,
+    model=model,
+    year=year,
+    color=color
+)
+{
+
+
+}
+
+class Minivan(mark: String, model: Int, year: Int, color: String, val enginePower:Int): Car(
+    mark=mark,
+    model=model,
+    year=year,
+    color=color
 )
 {
 
